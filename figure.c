d133b7c800e92fcd68a67a626f3d60f205cc0cab
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-10-30 03:18:24 +0100

    figure

---
+++ figure.c
@@ -0,0 +1,17 @@
+/* stampa figure geometriche */
+
+#include <stdio.h>
+
+int main( void ){
+
+  printf( "********    ****          \n" );
+  printf( "*      *    *  *          \n" );
+  printf( "*      *    *  *          \n" );
+  printf( "*      *    *  *    ***** \n" );
+  printf( "*      *    *  *    *   * \n" );
+  printf( "*      *    *  *    *   * \n" );
+  printf( "********    ****    ***** \n" );
+
+
+  return 0;
+}
