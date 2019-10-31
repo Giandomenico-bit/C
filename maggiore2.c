c6dbc57b1fffb5a981cee162bc81f07722964463
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-10-31 18:08:55 +0100

    maggiore2.c

--- maggiore2.c
+++ maggiore2.c
@@ -5,15 +5,18 @@
 int main( void ){
 
   int a, b;
+  int max;
 
   puts( "Inserisci primo numero : " );
   scanf( "%d", &a );
   puts( "Inserisci primo numero : " );
   scanf( "%d", &b );
 
-  if( a > b )
-    printf( "Maggiore : %d\n", a );
-  printf( "Maggiore : %d\n", b );
+  max = a;
+  if( b > a )
+    max = b;
+  printf( "il massimo e' : %d\n", max );
+
 
   return 0;
 }
