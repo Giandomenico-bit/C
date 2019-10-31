8a41697cd128192f0751e40450d15ae8e2f0c8e4
Giandomenico-bit <giandomenicoiameo00@gmail.com>
2019-10-31 18:13:36 +0100

    operazioni.c

--- operazioni.c
+++ operazioni.c
@@ -10,11 +10,15 @@ int main( void ){
   scanf( "%d", &a );
   puts( "Inserisci primo valore : " );
   scanf( "%d", &b );
+  puts( "" );
 
   printf( "Addizione  : %3d\n", a + b );
   printf( "Differenza : %3d\n", a - b );
   printf( "Prodotto   : %3d\n", a * b );
   printf( "Quoziente  : %3d\n", a / b );
+  printf( "Modulo     : %3d\n\n", a % b );
+
+  printf( "Sono 5\n" );
 
   return 0;
 }
